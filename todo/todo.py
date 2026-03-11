043f06c16a0de00a20f54beaa68dd2c10eeeedb7
SethWolf97 <wolfseth1997@gmail.com>
2026-03-10 22:09:23 -0500

    Adding file saving

--- todo/todo.py
+++ todo/todo.py
@@ -1,8 +1,16 @@
+# todo.py
+# by Seth Wolf
+# March 9th, 2026
+# Fills in a to-do list
+
 tasks = []
 
 while True:
     command = input("Give a command (add/list/quit): ")
     if command.lower() == "quit":
+        with open("list.txt", "w") as f:
+            for i, task in enumerate(tasks):
+                f.write(f"{i+1}. {task}\n")
         print("\nGoodbye")
         break
     elif command.lower() == "add":
