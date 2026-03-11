c27ccd31be5eb432b90dfae649ae80e81d3eb5fe
SethWolf97 <wolfseth1997@gmail.com>
2026-03-10 22:18:21 -0500

    Used a match-case statement

--- todo/todo.py
+++ todo/todo.py
@@ -3,24 +3,24 @@
 # March 9th, 2026
 # Fills in a to-do list
 
-tasks = []
+tasks = [] # list to fill with tasks
 
 while True:
     command = input("Give a command (add/list/quit): ")
-    if command.lower() == "quit":
-        with open("list.txt", "w") as f:
+    match(command.lower()):
+        case("quit"):
+            with open("list.txt", "w") as f:
+                for i, task in enumerate(tasks):
+                    f.write(f"{i+1}. {task}\n")
+            print("\nGoodbye")
+            break
+        case("add"):
+            task = input("Enter a task: ")
+            tasks.append(task)
+        case("list"):
+            print("\nTo-Do List")
             for i, task in enumerate(tasks):
-                f.write(f"{i+1}. {task}\n")
-        print("\nGoodbye")
-        break
-    elif command.lower() == "add":
-        task = input("Enter a task: ")
-        tasks.append(task)
-        print("Task added.")
-    elif command.lower() == "list":
-        print("\nTo-Do List")
-        for i, task in enumerate(tasks):
-            print(f"{i+1}. {task}")
-        print()
-    else:
-        print("Unknown command.")
+                print(f"{i+1}. {task}")
+            print()
+        case _:
+            print("Unknown command.")
